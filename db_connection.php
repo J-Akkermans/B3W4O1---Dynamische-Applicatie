45d29aad95c48a3f5519a3530ad831b500d0aa91
J Akkermans <99044668@mydavinci.nl>
2021-03-16 17:58:06 +0100

    done
    Only need to tweak some bits of the style.

--- db_connection.php
+++ db_connection.php
@@ -3,6 +3,4 @@ $dsn = "mysql:host=localhost;dbname=week_4_5_eind_opdracht";
 $user = "root";
 $passwd = "mysql";
 $pdo = new PDO($dsn, $user, $passwd);
-
-
 ?>
--- index.php
+++ index.php
@@ -1,18 +1,8 @@
 <?php
     include 'db_connection.php';
-
-
-
-
-        $data = $pdo->query( "SELECT `name`,`avatar`,`health`,`bio`,`color`,`defense`,`weapon`,`armor` FROM `characters`" );
-        $CharData = $data->fetch();
-
-
-        while($CharData = $data->fetch())
-        {
-                echo $CharData[name];
-
-        }
+    $amount = $pdo->query( "SELECT count(id) as AMOUNT FROM `characters`" );
+    $COUNT = $amount->fetch();
+    $data = $pdo->query( "SELECT `id`,`name`,`attack`,`avatar`,`health`,`color`,`defense`,`weapon`,`armor` FROM `characters`" );
 
 ?>
 
@@ -28,24 +18,32 @@
 <header><h1>Alle <?php echo $aantal ?> characters uit de database</h1>
 
 </header>
+<?php 
+while($CharData = $data->fetch())
+{
+    
+    ?>
 <div id="container">
-    <a class="item" href="character.html">
+    <a class="item" href="character.php?id=<?php echo $CharData['id'] ?>">
         <div class="left">
-            <img class="avatar" src="resources/images/bowser.jpg">
+            <img class="avatar" src="resources/images/<?php echo $CharData['avatar'] ?>">
         </div>
         <div class="right">
-            <h2>Bowser</h2>
+            <h2><?php echo $CharData['name'] ?></h2>
             <div class="stats">
                 <ul class="fa-ul">
-                    <li><span class="fa-li"><i class="fas fa-heart"></i></span> 10000</li>
-                    <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span> 400</li>
-                    <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span> 100</li>
+                    <li><span class="fa-li"><i class="fas fa-heart"></i></span> <?php echo $CharData['health'] ?></li>
+                    <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span> <?php echo $CharData['attack'] ?></li>
+                    <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span> <?php echo $CharData['defense'] ?></li>
                 </ul>
             </div>
         </div>
         <div class="detailButton"><i class="fas fa-search"></i> bekijk</div>
     </a>
 </div>
-<footer>&copy; [jenaam] 2020</footer>
+<?php
+}
+?>
+<footer>&copy; Jenna Akkermans 2021</footer>
 </body>
 </html>
